6fce7746c1e12dbd51f0c8c9c160ecacd30d41b1
Charlene Gipulan <charlenegipulan@gmail.com>
2020-06-17 16:33:41 -0700

    update style and create readme

--- Countdown clock/app.js
+++ Countdown clock/app.js
@@ -1,7 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelectorAll('[data-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
     //clear any existing timers
@@ -34,7 +34,7 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp) {
     const end = new Date(timestamp);
-    const hour = end.getHours();
+    const hour = end.getHours() % 12;
     const adjustedHour = hour > 12 ? hour - 12 : hour;
     const minutes = end.getMinutes();
     endTime.textContent = `Be back at ${adjustedHour} : ${minutes < 10 ? '0' : ''}${minutes}`;
---
+++ Countdown clock/counter-app.md
@@ -0,0 +1,4 @@
+Timer application that allows you to put in desired minutes, or select from the default options.
+
+
+<img src="https://i.imgur.com/WEjH4te.png" alt="timer-img">
